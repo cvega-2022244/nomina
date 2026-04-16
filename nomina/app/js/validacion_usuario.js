5f67e22db4b7f4e36b3469e36c02fbd371c2eb1c
jyaxon-2019386 <juanpabloyaxon@gmail.com>
2026-04-16 14:49:57 -0600

    Remuevo carpeta duplicada Nomina

--- nomina/app/js/validacion_usuario.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    if (!sessionStorage.getItem('usuario_principal') || sessionStorage.getItem('usuario_principal') == '' || sessionStorage.getItem('usuario_principal') == 'null') {
-        window.location.href = 'login.html'
-    }
-});
